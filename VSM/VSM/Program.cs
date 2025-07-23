d7f1ce48ec8bd2104d348f6abc1c455013b1270d
kaspar Lund <679479+kasparlund@users.noreply.github.com>
2025-07-23 13:37:34 +0200

    trying to commit againg

---
+++ VSM/VSM/Program.cs
@@ -0,0 +1,44 @@
+
+using Microsoft.FluentUI.AspNetCore.Components;
+using VSM.Client.Pages;
+using VSM.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents(); //.AddInteractiveWebAssemblyRenderMode();
+
+
+//builder.Services.AddHttpClient();  #create problems for SignalR
+builder.Services.AddFluentUIComponents();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(VSM.Client._Imports).Assembly);
+
+
+app.Run();
